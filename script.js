ab839d6e247449ba4f78f0e7ad9ac056ae6a7498
JaggedHorzion <rickrtimmers@gmail.com~echo>
2022-11-18 13:10:07 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function generateTarget() {
+    return Math.floor( (Math.random() * 9) + 1);
+    
+}
+
+function compareGuesses(currentHumanGuess, computerGuess, target) {
+    if (Math.abs((currentHumanGuess - target)) <= Math.abs((computerGuess - target))) {
+    return true
+    } else if (Math.abs((currentHumanGuess - target)) === Math.abs((computerGuess - target))) {
+        return true 
+    } else (Math.abs((currentHumanGuess - target)) >= Math.abs((computerGuess - target))); {
+        return false
+    }
+    }
+
+    function updateScore(winner) {
+        if (winner === 'computer') {
+           computerScore += 1
+        } else if (winner === 'human'){ 
+           humanScore += 1
+        }
+    }
+
+function advanceRound() {
+    currentRoundNumber += 1
+}
+
